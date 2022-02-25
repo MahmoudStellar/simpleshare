5af87a2cb38bf8adb8eee1e1142d507d2c5d4593
Mahmoud Atef <MahmoudStellar@yahoo.com>
2022-02-25 20:06:58 +0200

    status bar text color

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: SystemUiOverlayStyle(
-          statusBarColor: Colors.transparent,
-          statusBarBrightness: Brightness.dark),
+        statusBarColor: Colors.transparent,
+        // statusBarBrightness: Brightness.dark
+      ),
       child: MaterialApp(
         title: 'SimpleShare',
         theme: ThemeData(
           primaryColor: Colors.deepPurple,
           colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple)
               .copyWith(secondary: Colors.teal),
+          appBarTheme: AppBarTheme(brightness: Brightness.dark),
         ),
         debugShowCheckedModeBanner: false,
         home: Home(),
